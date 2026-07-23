e78f94c6fefc6774750ef7879cfccca5aaa30bd7
israel2002551 <israelefe093@gmail.com>
2026-07-23 22:08:34 +0100

    Configure Supabase project

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,4 +1,4 @@
 window.MHFF_SUPABASE_CONFIG = {
-  url: "",
-  anonKey: ""
+  url: "https://gtiriplaehlgxgztszpq.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0aXJpcGxhZWhsZ3hnenRzenBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4MzMxOTYsImV4cCI6MjEwMDQwOTE5Nn0.MHlTMHDUMlZy0ghyhXfyQHL4aaIkgnlCjv3BzvWYgWE"
 };
